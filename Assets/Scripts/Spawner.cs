a4457c9d7bfd3fbecc19b16496f9870355cbd5f1
SunDuck1992 <101988028+SunDuck1992@users.noreply.github.com>
2023-01-25 20:24:58 +0300

    Init_3
    Изменил название скрипта.

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private Enemy _enemy;
+    [SerializeField] private Transform[] _pointsOfSpawn;
+    [SerializeField] float _secondsBetweenSpawn;
+
+    private void Start()
+    {
+       StartCoroutine(Spawn());
+    }
+
+    private IEnumerator Spawn()
+    {
+        var waitForSeconds = new WaitForSeconds(_secondsBetweenSpawn);
+
+        foreach (var spawnPoint in _pointsOfSpawn)
+        {
+            Instantiate(_enemy, spawnPoint.transform.position, Quaternion.identity);
+            yield return waitForSeconds;
+        }
+    }
+}
